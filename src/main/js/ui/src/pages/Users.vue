b74d613622eef4596844b23dc31d805f00971e05
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2022-04-01 19:27:10 +0000

    Initial commit

---
+++ src/main/js/ui/src/pages/Users.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-page class="q-pa-md">
+        <q-table :columns="userColumns" :rows="users" :loading="loading" :pagination="pagination">
+            
+        </q-table>
+    </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'UserPage',
+    data() {
+        return {
+
+        };
+    }
+});
+</script>
